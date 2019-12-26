b22617e89b3a5de5f5832eb04a84fa0c22825206
近藤 勲 <isaokondo@nifty.com>
2019-12-26 15:12:16 +0900

    組立工程の編集→確認に変更

--- FLEX.NET/frmAssemblyProcessEdit.vb
+++ FLEX.NET/frmAssemblyProcessEdit.vb
@@ -23,17 +23,17 @@ Public Class frmAssemblyProcessEdit
     ''' </summary>
     ''' <param name="sender"></param>
     ''' <param name="e"></param>
-    Private Sub btnApply_Click(sender As Object, e As EventArgs) Handles btnApply.Click, btnOK.Click
+    Private Sub btnApply_Click(sender As Object, e As EventArgs) Handles btnApply.Click ', btnOK.Click
         CtlPara.AssemblyPieceNumber = AssemblyPieceNumber.Value
         SegAsmblyData.SegmentAsemblyDataUpdat(ConfirmRingNo.Value, AssemblyPatternSel.ComboBox.SelectedItem, DspSegmentTypeName.ComboBox.SelectedItem, Nothing)
         ConfirmRingNo_ValueChanged(sender, e)
     End Sub
 
-    Private Sub btnOK_Click(sender As Object, e As EventArgs) Handles btnOK.Click
-        'SegAsmblyData.SegmentAsemblyDataUpdat(ConfirmRingNo.Value, AssemblyPatternSel.ComboBox.SelectedItem, DspSegmentTypeName.ComboBox.SelectedItem, Nothing)
-        'ConfirmRingNo_ValueChanged(sender, e)
-        Me.Close()
-    End Sub
+    'Private Sub btnOK_Click(sender As Object, e As EventArgs) Handles btnOK.Click
+    '    'SegAsmblyData.SegmentAsemblyDataUpdat(ConfirmRingNo.Value, AssemblyPatternSel.ComboBox.SelectedItem, DspSegmentTypeName.ComboBox.SelectedItem, Nothing)
+    '    'ConfirmRingNo_ValueChanged(sender, e)
+    '    Me.Close()
+    'End Sub
 
     Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
 
